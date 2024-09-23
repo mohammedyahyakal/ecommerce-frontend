8b495d28efe6f972977d94bf7ea8093dd5064225
mohammedyahyakal <98487400+mohammedyahyakal@users.noreply.github.com>
2024-09-23 19:39:09 +0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCSZFYPN-4BeURMMpdFUc9veHg0_vrwnWg",
+ apiKey: "AIzaSyCSZFYPN-4BeURMMpdFUc9veHg0_vrwnWg",
   authDomain: "our-shop-e141d.firebaseapp.com",
   projectId: "our-shop-e141d",
   storageBucket: "our-shop-e141d.appspot.com",
   messagingSenderId: "757518928179",
-  appId: "1:757518928179:web:233843ef10a6c8f46d1646",
-  measurementId: "G-EDM7KLERWE"
+  appId: "1:757518928179:web:a720a0cc5fc0b2266d1646",
+  measurementId: "G-41BEDMRM5R"
 };
 
 // Initialize Firebase
